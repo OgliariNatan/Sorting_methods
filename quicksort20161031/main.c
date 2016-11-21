64fe7910d157939fe064643e09bc36df65ff85a4
OgliariNatan <natan.o@aluno.ifsc.edu.br>
2016-11-21 08:50:33 -0200

    Update main.c
    Update com 20 ordenação

--- quicksort20161031/main.c
+++ quicksort20161031/main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -9,11 +10,12 @@
 #include "med_3.h"
 
 //#define DEBUG
-#define TAM 10
+#define TAM 10000000     //!< Ideal 1000000
+#define VEZORDENACAO 20 //!< Ideal 20
 
 int main(){
 
-     int x=0;
+     int x=0, i;
      int *vetor;
      float med;
      clock_t inicio, fim;
@@ -33,12 +35,21 @@ int main(){
       medianaDeTres(vetor, 0, TAM-1);
       puts("----------------");
 
-     inicio = clock();
-     quicksort(vetor, 0, TAM);
-     fim = clock();
-     med = ((float) fim - (float) inicio ) / CLOCKS_PER_SEC;
-     printf("media %.4f\n\n",med);
+     for (i=1; i<=VEZORDENACAO; i++){
+          for (x=0; x<TAM; x++){
+               vetor[x] = rand();
+               #ifdef DEBUG
+                    printf("%d\n",vetor[x]);
+               #endif // DEBUG
+          }
+          inicio = clock();
+          quicksort(vetor, 0, TAM);
+          fim = clock();
+          med = ((float) fim - (float) inicio ) / CLOCKS_PER_SEC;
+          printf("media[%d] %f\n",i, med);
+     }
 
+     puts("----------------");
      #ifdef DEBUG
           puts("O vetor ordenado: ");
           for (x=0; x<TAM; x++){
