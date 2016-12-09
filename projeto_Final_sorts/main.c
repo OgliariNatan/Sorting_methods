1024bd4963fb33257c53f3d77cee7e26f9043130
OgliariNatan <natan.o@aluno.ifsc.edu.br>
2016-12-09 13:57:36 -0200

    Heap Sort em andamento

--- projeto_Final_sorts/main.c
+++ projeto_Final_sorts/main.c
@@ -21,11 +21,11 @@
 //#define DEBUGSELECTION
 #define DEBUGHEAPSORT //!<FALTA
 //#define DEBUGMERGE
-#define DEBUG
+//#define DEBUG
 
 
-#define TAM 10000    //!< Ideal 1000000
-#define VEZORDENACAO 10 //!< Ideal 20
+#define TAM 1000000    //!< Ideal 1000000
+#define VEZORDENACAO 3 //!< Ideal 20
 #define TAMEXPORTA 501 //!< Define a quantidade de elementos a serem exportados
 
 int main(){
@@ -140,14 +140,14 @@ int main(){
 
 #ifdef DEBUGHEAPSORT //!< Seleção HEAPSORT
     puts("Metodo Heap Sort");
-    puts("NAO IMPLEMENTADO");
+    puts("EM IMPLEMENTACAO");
     heap_t *meu_heap;
     meu_heap = cria_heap(vetor, TAM);
     for (i=0; i<VEZORDENACAO; i++){
         randomico(vetor,TAM);
 
         inicio = clock();
-        //heapSort(vetor, TAM);
+        //heapSort(meu_heap);
         fim = clock();
 
         tempo = ((float) fim - (float) inicio ) / CLOCKS_PER_SEC;
